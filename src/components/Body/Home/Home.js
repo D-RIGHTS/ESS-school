57839b276b2e1df094c25514a8ecb6a83313a2e3
D-RIGHTS <61801737+D-RIGHTS@users.noreply.github.com>
2021-02-05 14:20:23 +0200

    Finalized SignUp

--- src/components/Body/Home/Home.js
+++ src/components/Body/Home/Home.js
@@ -4,12 +4,14 @@ import HeroSection from "./HeroSection/HeroSection";
 import LevelCards from "./LevelCards/LevelCards";
 import Blog from "./Blog/Blog";
 
+
 function Home () {
     return(
         <div className="Home-styles">
            <HeroSection />
            <LevelCards />
            <Blog />
+
 
         </div>
     )
